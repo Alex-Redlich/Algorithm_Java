ee51616a1c520f5212c8cad9ca31496e7993324c
KIM YOUNGMIN <122436515+Alex-Redlich@users.noreply.github.com>
2024-01-16 14:59:52 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17968 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double A = input.nextDouble();
+        double B = input.nextDouble();
+
+        System.out.println(A/B);
+
+        input.close();
+    }
+}
