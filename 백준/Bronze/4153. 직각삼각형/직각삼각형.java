45e44796b1c51141a36c2b8d4b67397c764294f5
KIM YOUNGMIN <122436515+Alex-Redlich@users.noreply.github.com>
2024-01-25 13:17:51 +0900

    [Bronze III] Title: 직각삼각형, Time: 212 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 1. 배열 선언 및 입력값 담아두기
+        int[] arr = new int [3];
+        while (true) {
+            arr[0] = sc.nextInt();
+            arr[1] = sc.nextInt();
+            arr[2] = sc.nextInt();
+            Arrays.sort(arr);
+
+            if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0) {
+                break;
+            } else if (arr[2] * arr[2] == (arr[0] * arr[0]) + (arr[1] * arr[1])) {
+                System.out.println("right");
+            } else {
+                System.out.println("wrong");
+            }
+        }
+        sc.close();
+    }
+}
